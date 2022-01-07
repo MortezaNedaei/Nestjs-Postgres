a7cdb512d8377419f26038a48a916f76477ef808
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-08 01:08:57 +0330

    add swagger docs

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,7 +13,7 @@ import { Constants } from '../constants/Constants';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags(Constants.Routes.Coffees)
 @Controller(Constants.Routes.Coffees)
@@ -21,12 +21,14 @@ export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get(Constants.Routes.Flavors)
+  @ApiOperation({ summary: 'Retrieves all coffees' })
   findAll(@Query() paginationQuery: { offset: number; limit: number }) {
     const { offset, limit } = paginationQuery || {};
     return this.coffeesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'gets one coffee by id' })
   findOne(@Param('id') id: string) {
     const coffee = this.coffeesService.findOne(id);
     if (!coffee) {
@@ -36,16 +38,19 @@ export class CoffeesController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Creates new coffee' })
   create(@Body() dto: CreateCoffeeDto) {
     return this.coffeesService.create(dto);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Updates a coffee by id and new object' })
   update(@Param('id') id: string, @Body() dto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, dto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Removes a coffee by id' })
   delete(@Param('id') id: string) {
     return this.coffeesService.delete(id);
   }
