722919ef0310014c98a49b72bd7a4582dc1ce19c
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-09 23:44:16 +0330

    use connection and queryRunner to perform a transaction

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migration/*.js'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
