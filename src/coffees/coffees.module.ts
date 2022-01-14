62a73d43201206486c82b463348bd132cb1a1407
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-15 00:24:02 +0330

    add custom provider using useClass for the different config services in development and production environments. useClass used to determine a class that is related to token provide.

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,12 +7,16 @@ import { Flavor } from './entities/flavor.entity';
 import { Event } from './entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    { provide: ConfigService, useValue: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService },
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
   ],
   exports: [CoffeesService],
