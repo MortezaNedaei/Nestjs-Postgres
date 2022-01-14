2b72e90e7718049fd2e7ff4ab87687f174c06f69
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-15 00:04:59 +0330

    export and encapsulate CoffeesService to access properly from other modules

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -10,5 +10,6 @@ import { Event } from './entities/event.entity';
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
