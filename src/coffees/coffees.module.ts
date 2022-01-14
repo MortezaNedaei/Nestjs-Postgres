75091a1beef3c6d797161d62c534488f8079943b
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-15 00:43:56 +0330

    add async factory in custom provider to ensure that some task like establishing the db connection and querying data being done first successfully

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,18 +6,38 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from './entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
+import { Connection } from 'typeorm';
 
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
+// class ConfigService {}
+// class DevelopmentConfigService {}
+// class ProductionConfigService {}
+
+// @Injectable()
+// export class CoffeesBrandsFactory {
+//   create() {
+//     // do something
+//     return ['buddy brew', 'nescafe'];
+//   }
+// }
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    { provide: ConfigService, useValue: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService },
-    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+    // { provide: ConfigService, useValue: process.env.NODE_ENV === 'development' ? DevelopmentConfigService: ProductionConfigService, },
+    // CoffeesBrandsFactory,
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const coffeesBrands = await connection.query('SELECT * ...');
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        console.log('Async Factory');
+        return coffeeBrands;
+      },
+      // useFactory: (brandsFactory: CoffeesBrandsFactory) => brandsFactory.create(),
+      // inject: [CoffeesBrandsFactory],
+    },
   ],
   exports: [CoffeesService],
 })
