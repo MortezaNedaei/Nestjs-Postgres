778d95601d817a3fc326670c2028b857f100651f
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-15 21:41:01 +0330

    use singleton scope for the service lifecycle

--- src/coffee-rating/coffee-rating.service.ts
+++ src/coffee-rating/coffee-rating.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope } from '@nestjs/common';
 import { CoffeesService } from '../coffees/coffees.service';
 
-@Injectable()
+@Injectable({ scope: Scope.DEFAULT })
 export class CoffeeRatingService {
   constructor(private readonly coffeesService: CoffeesService) {}
 }
