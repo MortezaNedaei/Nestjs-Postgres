244b96daa4ab1f895babe1b0eb38cbd426d833de
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-17 23:49:49 +0330

    add guard for the APIs

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -1,11 +1,25 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler);
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.header('Authorization');
+    return authHeader === this.configService.get('API_KEY');
   }
 }
