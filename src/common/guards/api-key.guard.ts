5a2baa77e81909f3b70532bc58678738920d1cf2
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-17 23:53:23 +0330

    fix issue in guard

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -13,7 +13,7 @@ export class ApiKeyGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler);
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
     if (isPublic) {
       return true;
     }
