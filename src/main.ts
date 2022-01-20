18b0f23c7c263f647c4c34c2fbea5316f5b6bfd6
Morteza Nedaei <nedaei1375@gmail.com>
2022-01-20 15:55:20 +0330

    add global interceptor for base response

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './utils/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { ApiKeyGuard } from './common/guards/api-key.guard';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
